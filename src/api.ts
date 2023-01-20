30308bcf27041671406070586db1df45ba3a8dea
Artemis <34631859+Lukaribou@users.noreply.github.com>
2023-01-20 15:14:39 +0100

    fix post parameters to receive bad credentials page

--- src/api.ts
+++ src/api.ts
@@ -1,23 +1,21 @@
 import axios from "axios";
+import FormData = require('form-data');
 
 interface ApiCredentials {
     username: string,
     password: string
 };
 
-export default class GpuApi {
+export class GpuApi {
 
     public static login(credentials: ApiCredentials): Promise<any> {
-        const formData = JSON.stringify({
-            modeconnect: 'connect',
-            util: credentials.username,
-            acct_pass: credentials.password
-        });
+        const formData = new FormData();
+        formData.append('modeconnect', 'connect');
+        formData.append('util', credentials.username);
+        formData.append('acct_pass', credentials.password);
         
         return axios.post('https://www.gpu-lr.fr/sat/index.php?page_param=accueilsatellys.php', formData, {
-            headers: {
-                'Content-Type': 'application/json'
-            }
+            headers: formData.getHeaders()
         });
     }
 }
